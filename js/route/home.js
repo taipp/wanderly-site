6bb839ae6e745823267ff9506012edb4fb74d1bc
taipp <clipsrealbond@gmail.com>
2025-09-23 21:19:41 +0300

    add: main - home-page__top-slider | update: added slider library and updated section-search-tours__cards -> section-search-tours__slider-skew

--- js/route/home.js
+++ js/route/home.js
@@ -2,4 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     new Accordion( {
         $container: document.querySelector('#faq-accordion')
     });
+
+    new Swiper('#home-page-top-slider', {
+        slidesPerView: 4,
+        spaceBetween: 32,
+        // centeredSlides: true,
+        loop: true,
+    });
+
+    new Swiper('#section-search-tours__slider-skew', {
+        slidesPerView: 4,
+        spaceBetween: 32,
+        // centeredSlides: true,
+        loop: true,
+        autoplay: true,
+        speed: 750,
+    });
 });
+
